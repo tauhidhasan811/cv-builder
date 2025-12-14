54e3a8101026d0f078b5be90b9925f253605cfa1
tauhidhasan811 <tauhidhasan811@gmail.com>
2025-12-14 10:06:06 +0600

    add cv generator

--- component/services/cv_prompt.py
+++ component/services/cv_prompt.py
@@ -1,19 +1,19 @@
 from langchain_core.prompts import PromptTemplate
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
-def CVPrompt(user_text, user_data):
+def CVPrompt(user_data, additional_note):
     sys_message = SystemMessage(content="Imagine you are a professional CV writer. Your task is to create a detailed CV based on the provided job description and the user's personal bio data.")
     hum_message = HumanMessage(content=f"User Bio: \n{user_data}")
 
     temp = PromptTemplate(
-        template="{sys_message}\n. {hum_message} \n. Additional User Input: {user_text}",
-        input_variables=['sys_message', 'hum_message', 'user_text']
+        template="{sys_message}\n. {hum_message} \n. Additional User Input: {additional_note}",
+        input_variables=['sys_message', 'hum_message', 'additional_note']
     )
 
     prompt = temp.invoke(input={
          'sys_message': sys_message.content,
          'hum_message': hum_message.content,
-         'user_text': user_text
+         'additional_note': additional_note
     })
 
     return prompt.text
