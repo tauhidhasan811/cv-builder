919318327efad5f22e37f22295e208d3e320467c
tauhidhasan811 <tauhidhasan811@gmail.com>
2025-12-26 15:27:16 +0600

    audion model tested suggessfully

--- test.py
+++ test.py
@@ -2,9 +2,12 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-
+"""
 client = OpenAI()
-"""audio_file = open("audio.mp3", "rb")
+"""
+
+"""
+audio_file = open("audio.mp3", "rb")
 
 stream = client.audio.transcriptions.create(
   model="gpt-4o-mini-transcribe", 
@@ -14,8 +17,19 @@ stream = client.audio.transcriptions.create(
 )
 
 for event in stream:
-  print(event)"""
+  print(event)
+"""
+"""
 
 model = OpenAI().models
 
-print(model)
+print(model)"""
+
+from component.config.audio_model import OpenAIAudio
+
+model = OpenAIAudio()
+audio_path = "audio.mp3"
+
+text = model.ConvertToText(audio_path=audio_path)
+
+print(text)
