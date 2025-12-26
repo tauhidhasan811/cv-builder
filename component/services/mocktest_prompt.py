a0385d6ca7be6700078d31ffa894d16d59101987
tauhidhasan811 <tauhidhasan811@gmail.com>
2025-12-26 16:37:52 +0600

    improve mock test prompt

--- component/services/mocktest_prompt.py
+++ component/services/mocktest_prompt.py
@@ -2,15 +2,15 @@ from langchain_core.prompts import PromptTemplate
 from langchain.messages import SystemMessage, HumanMessage
 
 
-def MockTestPrompt(domain_name, num_of_question, def_level):
+def MockTestPrompt(domain_name, topic_name, num_of_question, def_level):
     out_temp = "[{'question': '<question_text>'}]"
 
     sys_message = SystemMessage(
         content=(
-            f"You are an examiner creating a mock test in the domain of {domain_name}. "
+            f"You are an examiner creating a mock test in the domain of {domain_name} and topic {topic_name}. "
             f"Generate exactly {num_of_question} questions. "
             f"The difficulty level of all questions must be {def_level}. "
-            f"All questions must be standard, relevant, and strictly limited to the {domain_name} domain. "
+            f"All questions must be standard, relevant, and strictly limited to the {domain_name} domain and topic {topic_name}."
             f"Do not include content from outside this domain and do not hallucinate facts. "
             f"Return ONLY valid list of JSON in the following format, with no extra text:\n"
             f"{out_temp}"
