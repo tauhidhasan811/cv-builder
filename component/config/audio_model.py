bd6659bc88c0bc08c7ac485f6d5239654902c940
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-07 12:47:13 +0600

    update audio model

--- component/config/audio_model.py
+++ component/config/audio_model.py
@@ -1,22 +1,48 @@
+# from openai import OpenAI
+
+# class OpenAIAudio:
+#     def __init__(self, model_name = "whisper-1"):
+#         self.client = OpenAI()
+#         self.model = model_name
+
+#     def ConvertToText(self, audio_path):
+#         """
+#         audion_file = open(audio_path, 'rb')
+#         result = self.client.audio.transcriptions.create(
+#             model=self.model,
+#             file=audion_file
+#         )
+#         """
+
+#         with open(audio_path, 'rb') as audion_file:
+#             result = self.client.audio.transcriptions.create(
+#                 model=self.model,
+#                 file=audion_file
+#             )
+#         return result.text
+
 from openai import OpenAI
 
 class OpenAIAudio:
-    def __init__(self, model_name = "whisper-1"):
+    def __init__(self, model_name="whisper-1"):
         self.client = OpenAI()
         self.model = model_name
 
     def ConvertToText(self, audio_path):
-        """
-        audion_file = open(audio_path, 'rb')
-        result = self.client.audio.transcriptions.create(
-            model=self.model,
-            file=audion_file
-        )
-        """
 
-        with open(audio_path, 'rb') as audion_file:
+        with open(audio_path, "rb") as audio_file:
             result = self.client.audio.transcriptions.create(
                 model=self.model,
-                file=audion_file
+                file=audio_file,
+                response_format="verbose_json"
             )
-        return result.text
+
+        segments = []
+        for seg in result.segments:
+            segments.append({
+                "start": seg.start,
+                "end": seg.end,
+                "text": seg.text
+            })
+
+        return segments
